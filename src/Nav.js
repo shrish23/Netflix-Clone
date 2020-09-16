a7e1152b3aa8c3009c1bea64bcd44175f5189811
shrish23 <shrishsharma@outlook.com>
2020-09-17 00:03:15 +0530

    Completed the netflix clone

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_blank"} `}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="NETFLIX LOGO"
+      />
+
+      <img
+        className="nav_avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="NETFLIX PROFILE"
+      />
+    </div>
+  );
+}
+
+export default Nav;
